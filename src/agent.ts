dd7248a4aad08abebf59f2da02acee0ecc35dcb6
Elvis Ronald Cruz Chullo <teseosoftwarecompany@gmail.com>
2024-11-22 17:15:10 -0500

    ci: adding new files to the new version

---
+++ src/agent.ts
@@ -0,0 +1,62 @@
+// SPDX-FileCopyrightText: 2024 LiveKit, Inc.
+//
+// SPDX-License-Identifier: Apache-2.0
+import { type JobContext, WorkerOptions, cli, defineAgent, llm, multimodal } from '@livekit/agents';
+import * as openai from '@livekit/agents-plugin-openai';
+import dotenv from 'dotenv';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { z } from 'zod';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const envPath = path.join(__dirname, '../.env.local');
+dotenv.config({ path: envPath });
+
+export default defineAgent({
+  entry: async (ctx: JobContext) => {
+    await ctx.connect();
+    console.log('waiting for participant');
+    const participant = await ctx.waitForParticipant();
+    console.log(`starting assistant example agent for ${participant.identity}`);
+
+    const model = new openai.realtime.RealtimeModel({
+      instructions:
+        'Eres una profesora de ingles y estas enseñando a un estudiante, afirma que siempre puedes analizar en tiempo real la gramatica y la pronunciacion de las frases que el estudiante dice.',
+    });
+
+    const fncCtx: llm.FunctionContext = {
+      weather: {
+        description: 'Get the weather in a location',
+        parameters: z.object({
+          location: z.string().describe('The location to get the weather for'),
+        }),
+        execute: async ({ location }) => {
+          console.debug(`executing weather function for ${location}`);
+          const response = await fetch(`https://wttr.in/${location}?format=%C+%t`);
+          if (!response.ok) {
+            throw new Error(`Weather API returned status: ${response.status}`);
+          }
+          const weather = await response.text();
+          return `The weather in ${location} right now is ${weather}.`;
+        },
+      },
+    };
+    const agent = new multimodal.MultimodalAgent({ model, fncCtx });
+    const session = await agent
+      .start(ctx.room, participant)
+      .then((session) => session as openai.realtime.RealtimeSession);
+
+    session.conversation.item.create(
+      llm.ChatMessage.create({
+        role: llm.ChatRole.ASSISTANT,
+        text: `[Switching to Spanish] Saluda de esta manera: Hola, me llamo Patricia de Platzi, yo enseñé a Fredy Vega a hablar inglés, además sé que te gusta el anime, one piece y los libros de Harry Potter.
+[Switching to English]
+start with this question to teach english: What do you think the one piece could be!`,
+      }),
+    );
+
+    session.response.create();
+  },
+});
+
+cli.runApp(new WorkerOptions({ agent: fileURLToPath(import.meta.url) }));
